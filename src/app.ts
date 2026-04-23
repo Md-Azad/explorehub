2352421936e5dbdab0c0d61a013be23d16911ff3
Md Azad Hossain <sokalazad@gmail.com>
2026-04-24 00:22:10 +0200

    added user module and routes and catchAsync funtion

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import express, { Request, Response } from "express";
+import { router } from "./app/routes";
 const app = express();
 
 app.use(express.json());
-
+app.use("/api/v1", router);
 app.get("/", (req: Request, res: Response) => {
-  res.status(200).send({ message: "Server is running for tour management." });
+  res.status(200).send({ message: "Server is running for explorehub." });
 });
 
 export default app;
---
+++ src/app/modules/users/user.controller.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from "express";
+
+import { userServices } from "./user.service";
+import { catchAsync } from "../../utils/catchAsync";
+
+const createUser = catchAsync(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async (req: Request, res: Response, next: NextFunction) => {
+    const result = await userServices.createUser(req.body);
+
+    res.send({
+      success: true,
+      statusCode: 201,
+      message: " users Created successfully",
+      data: result,
+    });
+  },
+);
+export const userController = {
+  createUser,
+};
