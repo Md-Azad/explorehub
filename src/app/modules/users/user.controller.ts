4e460ceceb0c880e9779024e19e1e6b04f327402
Md Azad Hossain <sokalazad@gmail.com>
2026-04-24 22:31:28 +0200

    edidted unnessery function

--- src/app/modules/users/user.controller.ts
+++ src/app/modules/users/user.controller.ts
@@ -1,11 +1,11 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 
 import { userServices } from "./user.service";
 import { catchAsync } from "../../utils/catchAsync";
 
 const createUser = catchAsync(
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  async (req: Request, res: Response, next: NextFunction) => {
+  async (req: Request, res: Response) => {
     const result = await userServices.createUser(req.body);
 
     res.status(201).send({
